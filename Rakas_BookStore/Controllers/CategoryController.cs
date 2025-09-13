cff126c99f9bcd6821da2b55dbfe57475d4cf7f1
Elvin Martinez <43117188+Elvin308@users.noreply.github.com>
2025-09-12 23:45:05 -0500

    Initial Creation of Category Page

---
+++ Rakas_BookStore/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rakas_BookStore.Data;
+using Rakas_BookStore.Models;
+
+namespace Rakas_BookStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categoryList  = _db.Categories.ToList();
+            return View(categoryList);
+        }
+    }
+}
