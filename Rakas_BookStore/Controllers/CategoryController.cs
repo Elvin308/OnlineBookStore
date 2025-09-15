9ccda45081b451b9c82392d708a0c0d1fd1ecc5c
Elvin Martinez <43117188+Elvin308@users.noreply.github.com>
2025-09-14 23:48:48 -0500

    Add Delete functionality

--- Rakas_BookStore/Controllers/CategoryController.cs
+++ Rakas_BookStore/Controllers/CategoryController.cs
@@ -76,5 +76,39 @@ namespace Rakas_BookStore.Controllers
                 return View();
             }
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+
+            Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category != null)
+            {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
     }
 }
