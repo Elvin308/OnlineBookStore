ad5c80fa5a242f205fd0d4beacd8e235aebec325
Elvin Martinez <43117188+Elvin308@users.noreply.github.com>
2025-09-17 13:17:21 -0500

    Add Category Specific Repository Interface implementing Repository Interface

---
+++ Rakas_BookStore.DataAccess/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Rakas_BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rakas_BookStore.DataAccess.Interfaces
+{
+    internal interface ICategoryRepository : IRepository<Category>
+    {
+        public void Update(Category cat);
+        public void Save();
+    }
+}
