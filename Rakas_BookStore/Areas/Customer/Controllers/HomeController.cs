fe906df9ed693e9a960d187c46bbb4c50eca2f08
Elvin Martinez <43117188+Elvin308@users.noreply.github.com>
2025-09-17 21:10:57 -0500

    Corrected Area configuration

--- Rakas_BookStore/Areas/Customer/Controllers/HomeController.cs
+++ Rakas_BookStore/Areas/Customer/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Rakas_BookStore.Models;
 
 namespace Rakas_BookStore.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
