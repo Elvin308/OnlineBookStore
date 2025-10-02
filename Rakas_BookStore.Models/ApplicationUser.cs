b27a757d39799c2da673da97df2cf902dd453392
Elvin Martinez <43117188+Elvin308@users.noreply.github.com>
2025-10-02 12:23:22 -0500

    Add additional fields to register page

--- Rakas_BookStore.Models/ApplicationUser.cs
+++ Rakas_BookStore.Models/ApplicationUser.cs
@@ -11,9 +11,9 @@ namespace Rakas_BookStore.Models
     public class ApplicationUser : IdentityUser
     {
         [Required]
-        public int Name { get; set; }
+        public string Name { get; set; }
 
-        public string? StreetAdress { get; set; }
+        public string? StreetAddress { get; set; }
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
